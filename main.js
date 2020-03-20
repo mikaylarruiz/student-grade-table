562a8f94e6436bb5f7e9482d567c6b2cc01337e3
Mikayla Ruiz <mikaylarruiz@gmail.com>
2020-03-20 16:32:18 -0700

    completed add a pageHeader component

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var table = document.querySelector('table');
 var gradeTable = new GradeTable(table);
 
-var app = new App(gradeTable);
+var header = document.querySelector('header');
+var pageHeader = new PageHeader(header);
+
+var app = new App(gradeTable, pageHeader);
 app.start();
 console.log(app);
