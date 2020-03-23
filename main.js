e8b1fc44774c0e8c9e4529c7287af8fb0e03312e
Mikayla Ruiz <mikaylarruiz@gmail.com>
2020-03-23 10:39:33 -0700

    completed Add a GradeForm Component

--- main.js
+++ main.js
@@ -4,5 +4,8 @@ var gradeTable = new GradeTable(table);
 var header = document.querySelector('header');
 var pageHeader = new PageHeader(header);
 
-var app = new App(gradeTable, pageHeader);
+var form = document.querySelector('form');
+var gradeForm = new GradeForm(form);
+
+var app = new App(gradeTable, pageHeader, gradeForm);
 app.start();
