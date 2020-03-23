33460bdd9786d0910c7951b763f27c3654f9f106
Mikayla Ruiz <mikaylarruiz@gmail.com>
2020-03-23 12:26:21 -0700

    completed enhance the app component of feature 4

--- components/app.js
+++ components/app.js
@@ -8,6 +8,9 @@ class App {
     this.createGrade = this.createGrade.bind(this);
     this.handleCreateGradeError = this.handleCreateGradeError.bind(this);
     this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this);
+    this.deleteGrade = this.deleteGrade.bind(this);
+    this.handleDeleteGradeError = this.handleDeleteGradeError.bind(this);
+    this.hanadleDeleteGradeSuccess = this.hanadleDeleteGradeSuccess.bind(this);
   }
   handleGetGradesError(error){
     console.error(error);
@@ -53,6 +56,15 @@ class App {
     console.error(error);
   }
   handleCreateGradeSuccess() {
+    this.getGrades();
+  }
+  deleteGrade(id){
+    console.log(id);
+  }
+  handleDeleteGradeError(error){
+    console.error(error);
+  }
+  hanadleDeleteGradeSuccess(){
     this.getGrades();
   }
 }
